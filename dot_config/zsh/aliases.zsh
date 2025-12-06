5b0da717daccf7b36287afa3e3eafe835a993f2b
Tal Atlas <me@tal.by>
2025-12-05 20:57:26 -0500

    added ngrok

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -6,28 +6,6 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-# Git aliases
-alias gs='git status'
-alias ga='git add'
-alias gc='git commit'
-alias gp='git push'
-alias gl='git log --oneline'
-alias gd='git diff'
-alias gb='git branch'
-alias gco='git checkout'
-
-# Chezmoi aliases
-alias cm='chezmoi'
-alias cma='chezmoi apply'
-alias cme='chezmoi edit'
-alias cmu='chezmoi update'
-alias cmd='chezmoi diff'
-
-# Brew aliases
-alias bi='brew install'
-alias bs='brew search'
-alias bu='brew update && brew upgrade'
-alias binfo='brew info'
-
 # Claude aliases
 alias cyolo='claude --allow-dangerously-skip-permissions'
+alias cc='claude --allow-dangerously-skip-permissions'
